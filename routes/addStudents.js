4957a75bc7324a7cde5941b6b58766696cafbbc1
Pratik <pratik.aimachine@gmail.com>
2021-02-18 11:43:41 +0530

    done

--- routes/addStudents.js
+++ routes/addStudents.js
@@ -3,6 +3,35 @@ const addStudentDetail = require('../models/addingStudents');
 
 
 
+router.get('/' , async (req, res) => {
+    try{
+        const addStudentDetails = await addStudentDetail.find();
+        res.json(addStudentDetails);
+    }
+    catch(err){
+        res.json({message:err});
+
+    }
+
+
+    
+});
+
+
+router.get('/:sid' , async (req, res) => {
+    try{
+        const addStudentDetails = await addStudentDetail.find({sid : req.params.sid});
+        res.json(addStudentDetails);
+    }   
+    catch(err){
+        res.json({message:err});
+    }
+    
+});
+
+
+
+
 router.post('/addStudentDetail' , async (req , res) => {
     const details = new addStudentDetail({
         sid : req.body.sid,
